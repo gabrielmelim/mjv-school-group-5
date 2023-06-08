6a6c89125d083ba7cb7babc053249fdcd5f0df96
FernandoVictorff <fernandovictor01@gmail.com>
2023-06-08 10:57:15 -0300

    Adicionado atributo nomeCliente e metodo cancelarConta

--- ContaCorrente.java
+++ ContaCorrente.java
@@ -1,9 +1,16 @@
-ss	public class ContaCorrente {
-		//Atributos
-		
-		
-		
-		//Métodos
-		
-	
-	}	
+public class ContaCorrente {
+    //Atributos
+    private final String nomeCliente;
+
+    public ContaCorrente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    //Métodos
+    public void cancelarConta(String justificativa) {
+     // deleteById(this.numeroConta); Metodo apenas para exemplo
+        System.out.println("Sua conta foi cancelada com sucesso!!");
+        System.out.println("Seu motivo -> " + justificativa);
+    }
+}
+
