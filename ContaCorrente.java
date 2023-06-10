05b9d87402d05f846290c1af468ba724fb3de96d
Jaque <jaqueline.santosearaujo@gmail.com>
2023-06-09 22:01:56 -0300

    Ajuste do atributo e do método conta.java

--- ContaCorrente.java
+++ ContaCorrente.java
@@ -1,11 +1,23 @@
-	public class ContaCorrente {
-		// Atributos
-		private double saldo;
-		
-		
-		// Métodos
-		public double consultarSaldo(){
-		return saldo;
-		}
+public class ContaCorrente {
+    private double saldo;
 
-	}
+    public ContaCorrente(double saldoInicial) {
+        saldo = saldoInicial;
+    }
+
+    public double consultarSaldo() {
+        return saldo;
+
+    }
+    public static void main(String[] args) {
+		ContaCorrente minhaConta = new ContaCorrente(1000.0);
+
+        double saldoAtual = minhaConta.consultarSaldo();
+        System.out.println("Saldo atual: "+ saldoAtual);
+
+
+    }
+
+}
+
+
